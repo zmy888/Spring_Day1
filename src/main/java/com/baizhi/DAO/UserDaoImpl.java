2553e6790be453d23568d901b3af528a340ddd0d
zmy888 <57437638+zmy888@users.noreply.github.com>
2019-11-06 20:35:33 +0800

    添加注释

--- src/main/java/com/baizhi/DAO/UserDaoImpl.java
+++ src/main/java/com/baizhi/DAO/UserDaoImpl.java
@@ -3,6 +3,8 @@ package com.baizhi.DAO;
 import com.baizhi.Entity.User;
 
 public class UserDaoImpl implements UserDao {
+    
+    //注册业务 参数为一个对象
     @Override
     public void register(User user) {
         System.out.println("我是UserDao实现类中的方法");
